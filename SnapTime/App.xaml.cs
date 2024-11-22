5f841ac45c02ffb1185d51946c47eaf66c89f719
MartijnW <weningmartijn@gmail.com>
2024-11-22 14:48:57 +0100

    Add project files.

---
+++ SnapTime/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SnapTime
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
