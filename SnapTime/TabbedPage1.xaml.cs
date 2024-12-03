de0e108535817dc6cddbbc4b47f3d9a3ef500eb2
MartijnW <weningmartijn@gmail.com>
2024-12-03 11:56:57 +0100

    Home is first page

--- SnapTime/TabbedPage1.xaml.cs
+++ SnapTime/TabbedPage1.xaml.cs
@@ -2,8 +2,10 @@ namespace SnapTime;
 
 public partial class TabbedPage1 : TabbedPage
 {
-	public TabbedPage1()
-	{
-		InitializeComponent();
-	}
+    public TabbedPage1()
+    {
+        InitializeComponent();
+            
+        CurrentPage = Children[1]; // Index 1 verwijst naar het tweede tabblad ("Home")
+    }
 }
